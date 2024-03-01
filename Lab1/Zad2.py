b1c1d7c915c332d28bd2a9336408458629a43eed
Berkel1611 <161635080+Berkel1611@users.noreply.github.com>
2024-03-01 17:22:08 +0100

    Labs1

---
+++ Lab1/Zad2.py
@@ -0,0 +1,2 @@
+x, y = int(input('Podaj pierwszą liczbę: ')), int(input('Podaj drugą liczbę: '))
+print("Wynik dodania {} i {} to {}".format(x, y, 2*x+5*y))
---
+++ Lab1/Zad3.py
@@ -0,0 +1,6 @@
+name = input('Podaj imię: ')
+surname = input('Podaj nazwisko: ')
+age = int(input('Podaj wiek: '))
+study = input('Podaj kierunek studiów: ')
+
+print('Jestem {} {} mam {} lat studiuję {}'.format(name, surname, age, study))
