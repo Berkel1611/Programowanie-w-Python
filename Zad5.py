a949de8e82d5da81550cb6c67bfae81d6e48430c
Berkel1611 <161635080+Berkel1611@users.noreply.github.com>
2024-03-01 17:13:55 +0100

    Labs1

---
+++ Zad5.py
@@ -0,0 +1,4 @@
+a = int(input('Podaj 1. liczbę: '))
+b = int(input('Podaj 2. liczbę: '))
+
+print('Suma {} i {} jest parzysta: {}'.format(a, b, (a+b) % 2 == 0))
---
+++ Zad6.py
@@ -0,0 +1,8 @@
+a = int(input('Podaj 1. liczbę: '))
+b = int(input('Podaj 2. liczbę: '))
+
+print('\nSuma: {}'.format(a+b))
+print('Różnica: {}'.format(a-b))
+print('Iloczyn: {}'.format(a*b))
+print('Iloraz: {}'.format(a/b))
+print('Potęga: {}'.format(a**b))
