64af350487a9eb6c8b40295557cae4a71f17520e
Berkel1611 <161635080+Berkel1611@users.noreply.github.com>
2024-03-01 17:15:39 +0100

    Delete Zad6.py

--- Zad6.py
+++
@@ -1,8 +0,0 @@
-a = int(input('Podaj 1. liczbę: '))
-b = int(input('Podaj 2. liczbę: '))
-
-print('\nSuma: {}'.format(a+b))
-print('Różnica: {}'.format(a-b))
-print('Iloczyn: {}'.format(a*b))
-print('Iloraz: {}'.format(a/b))
-print('Potęga: {}'.format(a**b))
