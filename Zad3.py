74a2ca64916d4745bfa364366a7b6795348da962
Berkel1611 <161635080+Berkel1611@users.noreply.github.com>
2024-03-01 17:15:16 +0100

    Delete Zad3.py

--- Zad3.py
+++
@@ -1,6 +0,0 @@
-name = input('Podaj imię: ')
-surname = input('Podaj nazwisko: ')
-age = int(input('Podaj wiek: '))
-study = input('Podaj kierunek studiów: ')
-
-print('Jestem {} {} mam {} lat studiuję {}'.format(name, surname, age, study))
